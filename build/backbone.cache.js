1a2c32396ea0e8f0f8e93e99446779515d6b459f
Makis Tracend <makis.tracend@gmail.com>
2015-01-11 16:53:56 -0800

    :gem: 0.3.0

--- build/backbone.cache.js
+++ build/backbone.cache.js
@@ -1,51 +1,46 @@
-// @name backbone.cache - 0.2.0 (Wed, 12 Mar 2014 07:08:05 GMT)
-// @url http://github.com/makesites/backbone-cache
 
-// @author makesites
-// @license MIT license
 
-(function(_, Backbone) {
+/**
+ * @name backbone.cache
+ * Uniform data caching for Backbone.js apps
+ *
+ * Version: 0.3.0 (Mon, 12 Jan 2015 00:53:32 GMT)
+ * Source: http://github.com/makesites/backbone-cache
+ *
+ * @author makesites
+ * Initiated by: Makis Tracend (@tracend)
+ * Distributed through [Makesites.org](http://makesites.org)
+ *
+ * @cc_on Copyright © Makesites.org
+ * @license Released under the MIT
+ */
+
+(function (lib) {
+
+	//"use strict";
+
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		var deps = ['underscore', 'backbone'];
+		define(deps, lib); // give the module a name
+	} else if ( typeof module === "object" && module && typeof module.exports === "object" ){
+		// Expose as module.exports in loaders that implement CommonJS module pattern.
+		module.exports = lib;
+	} else {
+		// Browser globals
+		lib(window._, window.Backbone);
+	}
+}(function ( _, Backbone ) {
+
+	//"use strict";
+	// better way to define global scope?
+	var window = window || {};
+	var APP = window.APP || false;
 
 	// support for Backbone APP() view if available...
-	var isAPP = ( typeof APP !== "undefined" && typeof APP.View !== "undefined" );
-	var Parent = (isAPP ) ? APP : Backbone;
+	var isAPP = ( APP !== false );
+	var Parent = ( isAPP ) ? APP : Backbone;
 
-
-	var Model = Parent.Model.extend({
-
-		cache: function( data ){
-			// prerequisites
-			if( typeof localStorage == "undefined" ) return;
-			// currently only two actions are supported (get & set)
-			var name = this.options.cache_key || this.name || "model";
-			if( data ){
-				if( data[this.idAttribute] ) name += "_"+ data[this.idAttribute];
-				return this.store.set(name, JSON.stringify( data ) );
-			} else {
-				var cached = this.store.get(name);
-				return ( _.isNull( cached ) || _.isEmpty( cached ) ) ? {} : JSON.parse( cached );
-			}
-		},
-
-		// localStorage
-		store : {
-			get : function( name ) {
-					return localStorage.getItem( name );
-			},
-			set : function( name, val ){
-					// validation first?
-					return localStorage.setItem( name, val );
-			},
-			check : function( name ){
-					return ( localStorage.getItem( name ) === null );
-			},
-			clear: function( name ){
-					// actually just removing the local...
-					return localStorage.removeItem( name );
-			}
-		}
-
-	});
 
 
 	var Collection = Parent.Collection.extend({
@@ -100,6 +95,43 @@
 
 	});
 
+
+	var Model = Parent.Model.extend({
+
+		cache: function( data ){
+			// prerequisites
+			if( typeof localStorage == "undefined" ) return;
+			// currently only two actions are supported (get & set)
+			var name = this.options.cache_key || this.name || "model";
+			if( data ){
+				if( data[this.idAttribute] ) name += "_"+ data[this.idAttribute];
+				return this.store.set(name, JSON.stringify( data ) );
+			} else {
+				var cached = this.store.get(name);
+				return ( _.isNull( cached ) || _.isEmpty( cached ) ) ? {} : JSON.parse( cached );
+			}
+		},
+
+		// localStorage
+		store : {
+			get : function( name ) {
+					return localStorage.getItem( name );
+			},
+			set : function( name, val ){
+					// validation first?
+					return localStorage.setItem( name, val );
+			},
+			check : function( name ){
+					return ( localStorage.getItem( name ) === null );
+			},
+			clear: function( name ){
+					// actually just removing the local...
+					return localStorage.removeItem( name );
+			}
+		}
+
+	});
+
 //Views may have their html fragments cached (TBA)
 
 	var View = Parent.View.extend({
@@ -107,18 +139,7 @@
 
 	});
 
-	// fallbacks
 
-	// Support module loaders
-	if ( typeof module === "object" && module && typeof module.exports === "object" ) {
-		// Expose as module.exports in loaders that implement CommonJS module pattern.
-		module.exports = Parent;
-	} else {
-		// Register as a named AMD module, used in Require.js
-		if ( typeof define === "function" && define.amd ) {
-			define( [], function () { return Parent; } );
-		}
-	}
 	// If there is a window object, that at least has a document property
 	if ( typeof window === "object" && typeof window.document === "object" ) {
 
@@ -130,5 +151,7 @@
 
 	}
 
+	// for module loaders:
+	return APP;
 
-})(this._, this.Backbone);
+}));
